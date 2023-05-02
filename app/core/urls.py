5294737497c3e8489111ad9f5c102256a7655385
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-05-02 06:53:01 -0500

    create index app

--- app/core/urls.py
+++ app/core/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("", include("index.urls"), name="index"),
     path("admin/", admin.site.urls),
     path("brewery/", include("brewery.urls"), name="brewery"),
     path("beer/", include("beer.urls"), name="beer"),
