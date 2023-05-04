2ccc735d3805a8d7b9332b49580f6b3f8535e324
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-05-04 08:00:49 -0500

    add and extend models

--- app/beer/models.py
+++ app/beer/models.py
@@ -2,15 +2,43 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+TYPES = [
+    ("Ale", "Ale"),
+    ("Lager", "Lager"),
+    ("Hybrid", "Hybrid"),
+]
+
+# https://www.brewersassociation.org/edu/brewers-association-beer-style-guidelines
+# TODO: consider using table and foreign key rather than choices
+STYLES = [
+    ("Other", "Other"),
+]
+
 
 class Beer(models.Model):
     name = models.CharField(max_length=256)
     brewery = models.ForeignKey("brewery.Brewery", on_delete=models.CASCADE)
+    type = models.CharField(max_length=256, choices=TYPES)
+    style = models.CharField(max_length=256, choices=STYLES)
+    abv = models.FloatField(validators=[MaxValueValidator(100), MinValueValidator(0)])
+    ibu = models.FloatField(validators=[MaxValueValidator(120), MinValueValidator(0)])
+    description = models.TextField(max_length=4096, null=True)
+    color = models.CharField(max_length=256, null=True)
 
 
 class BeerReview(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     beer = models.ForeignKey(Beer, on_delete=models.CASCADE)
-    rating = models.IntegerField(
-        validators=[MaxValueValidator(5), MinValueValidator(1)],
-    )
+    rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    bitter = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
+    sweet = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
+    sour = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
+    carbonation = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
+    head = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
+    smell = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
+    text = models.TextField(max_length=4096)
+
+
+class BeerImage(models.Model):
+    beer = models.ForeignKey(Beer, on_delete=models.CASCADE)
+    path = models.CharField(max_length=256)
--- app/brewery/models.py
+++ app/brewery/models.py
@@ -1,10 +1,39 @@
+import datetime
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+BREWERY_TYPES = [
+    ("Brewery Only", "Brewery Only"),
+    ("Taproom", "Taproom"),
+    ("Brewpub", "Brewpub"),
+    ("Beer Garden", "Beer Garden"),
+    ("Other", "Other"),
+]
+
+CAPACITIES = [
+    ("< 10", "< 10"),
+    ("10-25", "10-25"),
+    ("25-50", "25-50"),
+    ("50-100", "50-100"),
+    ("> 100", "> 100"),
+]
+
+DISTRIBUTIONS = [
+    ("Cans", "Cans"),
+    ("Bottles", "Bottles"),
+    ("Kegs", "Kegs"),
+    ("Growlers", "Growlers"),
+    ("Draft", "Draft"),
+    ("Other", "Other"),
+]
+
 
 class Brewery(models.Model):
     name = models.CharField(max_length=256)
+    founded = models.IntegerField(
+        validators=[MaxValueValidator(datetime.date.today().year), MinValueValidator(1829)]  # Yuengling founded in 1829
+    )
 
 
 class BreweryReview(models.Model):
@@ -13,3 +42,19 @@ class BreweryReview(models.Model):
     rating = models.IntegerField(
         validators=[MaxValueValidator(5), MinValueValidator(1)],
     )
+
+
+class BreweryImage(models.Model):
+    brewery = models.ForeignKey(Brewery, on_delete=models.CASCADE)
+    path = models.CharField(max_length=256)
+
+
+class BreweryLocation(models.Model):
+    brewery = models.ForeignKey(Brewery, on_delete=models.CASCADE)
+    street = models.CharField(max_length=256)
+    city = models.CharField(max_length=256)
+    state = models.CharField(max_length=256)
+    type = models.CharField(max_length=256, choices=BREWERY_TYPES)
+    tours = models.BooleanField()
+    capacity = models.CharField(max_length=256, choices=CAPACITIES)
+    distribution = models.CharField(max_length=256, choices=DISTRIBUTIONS)
