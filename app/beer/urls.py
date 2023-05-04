b425dce49317c5d5eaf565bd332184f231219367
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-05-04 08:01:19 -0500

    login protect endpoints that affect db

--- app/beer/urls.py
+++ app/beer/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import views
@@ -6,9 +7,9 @@ app_name = "beer"
 
 urlpatterns = [
     path("", views.BeerListView.as_view(), name="beer_list"),
-    path("create/", views.BeerCreateView.as_view(), name="beer_create"),
+    path("create/", login_required(views.BeerCreateView.as_view()), name="beer_create"),
     path("<str:pk>/", views.BeerDetailView.as_view(), name="beer_detail"),
-    path("update/<str:pk>/", views.BeerUpdateView.as_view(), name="beer_update"),
-    path("delete/<str:pk>/", views.BeerDeleteView.as_view(), name="beer_delete"),
-    path("<str:pk>/review/create/", views.ReviewCreateView.as_view(), name="review_create"),
+    path("update/<str:pk>/", login_required(views.BeerUpdateView.as_view()), name="beer_update"),
+    path("delete/<str:pk>/", login_required(views.BeerDeleteView.as_view()), name="beer_delete"),
+    path("<str:pk>/review/create/", login_required(views.ReviewCreateView.as_view()), name="review_create"),
 ]
--- app/brewery/urls.py
+++ app/brewery/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import views
@@ -6,9 +7,9 @@ app_name = "brewery"
 
 urlpatterns = [
     path("", views.BreweryListView.as_view(), name="brewery_list"),
-    path("create/", views.BreweryCreateView.as_view(), name="brewery_create"),
+    path("create/", login_required(views.BreweryCreateView.as_view()), name="brewery_create"),
     path("<str:pk>/", views.BreweryDetailView.as_view(), name="brewery_detail"),
-    path("update/<str:pk>/", views.BreweryUpdateView.as_view(), name="brewery_update"),
-    path("delete/<str:pk>/", views.BreweryDeleteView.as_view(), name="brewery_delete"),
-    path("<str:pk>/review/create/", views.ReviewCreateView.as_view(), name="review_create"),
+    path("update/<str:pk>/", login_required(views.BreweryUpdateView.as_view()), name="brewery_update"),
+    path("delete/<str:pk>/", login_required(views.BreweryDeleteView.as_view()), name="brewery_delete"),
+    path("<str:pk>/review/create/", login_required(views.ReviewCreateView.as_view()), name="review_create"),
 ]
