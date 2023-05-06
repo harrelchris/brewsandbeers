9957c1dc3349278499d1882c519d5b08610fb791
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-05-06 08:02:45 -0500

    refactor beer models

--- app/beer/models.py
+++ app/beer/models.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+
+from models import BaseModel
 
 TYPES = [
     ("Ale", "Ale"),
@@ -15,40 +16,87 @@ STYLES = [
 ]
 
 
-class Beer(models.Model):
+class Beer(BaseModel):
     name = models.CharField(max_length=256)
     brewery = models.ForeignKey("brewery.Brewery", on_delete=models.CASCADE)
     type = models.CharField(max_length=256, choices=TYPES)
     style = models.CharField(max_length=256, choices=STYLES)
-    abv = models.FloatField(validators=[MaxValueValidator(100), MinValueValidator(0)])
-    ibu = models.FloatField(validators=[MaxValueValidator(120), MinValueValidator(0)])
-    description = models.TextField(max_length=4096, null=True)
-    color = models.CharField(max_length=256, null=True)
+    description = models.TextField(max_length=4096, null=True, blank=True)
+    color = models.CharField(max_length=256, null=True, blank=True)
+    abv = models.FloatField(
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100),
+        ],
+    )
+    ibu = models.FloatField(
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(120),
+        ],
+    )
 
     def __str__(self):
         return f"{self.name} - {self.brewery.name}"
 
 
-class BeerReview(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    beer = models.ForeignKey(Beer, on_delete=models.CASCADE)
-    rating = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
-    bitter = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
-    sweet = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
-    sour = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
-    carbonation = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
-    head = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
-    smell = models.IntegerField(validators=[MaxValueValidator(10), MinValueValidator(0)])
-    text = models.TextField(max_length=4096)
-
-    def __str__(self):
-        return f"{self.beer.brewery.name} - {self.beer.name} by {self.user.username}"
-
-
-class BeerImage(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+class BeerImage(BaseModel):
     beer = models.ForeignKey(Beer, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="images/beer/")
 
     def __str__(self):
         return f"{self.beer.brewery.name} - {self.beer.name} - {self.pk}"
+
+
+class BeerReview(BaseModel):
+    beer = models.ForeignKey(Beer, on_delete=models.CASCADE)
+    text = models.TextField(max_length=4096)
+    rating = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5),
+        ],
+    )
+    bitter = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(10),
+        ],
+    )
+    sweet = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(10),
+        ],
+    )
+    sour = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(10),
+        ],
+    )
+    carbonation = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(10),
+        ],
+    )
+    head = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(10),
+        ],
+    )
+    smell = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(10),
+        ],
+    )
+
+    def __str__(self):
+        return f"{self.beer.brewery.name} - {self.beer.name} by {self.user.username}"
