ab80e2766a1f279213dc8663e3fd821caf26e80b
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-05-09 10:39:32 -0500

    add image model and view

--- app/location/urls.py
+++ app/location/urls.py
@@ -7,5 +7,6 @@ app_name = "location"
 urlpatterns = [
     path("<int:pk>/", views.LocationDetail.as_view(), name="location_detail"),
     path("<int:pk>/create/", views.LocationCreate.as_view(), name="location_create"),
+    path("<int:pk>/image/", views.ImageCreate.as_view(), name="image_create"),
     path("<int:pk>/review/", views.ReviewCreate.as_view(), name="review_create"),
 ]
