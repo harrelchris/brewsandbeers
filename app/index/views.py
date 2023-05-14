badc7c689a483151174e3ef307e3b722511dc3d4
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-05-14 11:00:37 -0500

    implement basic search

--- app/index/views.py
+++ app/index/views.py
@@ -1,4 +1,5 @@
 from itertools import chain
+from django.shortcuts import render, reverse
 from django.views.generic import TemplateView
 
 from beer.models import Beer, BeerImage, BeerReview
@@ -27,5 +28,41 @@ class Index(TemplateView):
         return context
 
 
-class Search(TemplateView):
-    pass
+def search(request):
+    query_string = request.GET.get("search")
+    if query_string:
+        beers = Beer.objects.filter(name__icontains=query_string)
+        breweries = Brewery.objects.filter(name__icontains=query_string)
+        locations = Location.objects.filter(brewery__name__icontains=query_string)
+
+        results = []
+        for beer in beers:
+            result = {
+                "text": beer.name,
+                "link": reverse("beer:beer_detail", kwargs={"pk": beer.pk}),
+                "type": "beer"
+            }
+            results.append(result)
+        for brewery in breweries:
+            result = {
+                "text": brewery.name,
+                "link": reverse("brewery:brewery_detail", kwargs={"pk": brewery.pk}),
+                "type": "brewery"
+            }
+            results.append(result)
+        for location in locations:
+            result = {
+                "text": f"{location.brewery.name} {location.city}, {location.state}",
+                "link": reverse("location:location_detail", kwargs={"pk": location.pk}),
+                "type": "location"
+            }
+            results.append(result)
+        context = {
+            "results": results
+        }
+        return render(
+            request=request,
+            template_name="index/search.html",
+            context=context,
+        )
+
