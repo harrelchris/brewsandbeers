5124d89b7a7f3fe23b964401bdacc910f4b8d3d0
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-05-15 18:45:06 -0500

    create tests directory

---
+++ app/core/tests/test_views.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+
+class TestViews(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        """Ran once before any test methods"""
+        pass
+
+    def setUp(self) -> None:
+        """Ran prior to every test method"""
+        pass
+
+    def tearDown(self) -> None:
+        """Ran following every test method"""
+        pass
+
+    def test_true(self):
+        return self.assertTrue(True)
+
+    def test_false(self):
+        return self.assertFalse(False)
+
+    def test_equal(self):
+        return self.assertEquals(1, 1)
